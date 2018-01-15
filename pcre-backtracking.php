8480d4f6babdc63bbb61f76d9df3cd184dcf7176
zouyi <php-cpm@users.noreply.github.com>
2018-01-15 10:39:54 +0800

    Update pcre-backtracking.php

--- pcre-backtracking.php
+++ pcre-backtracking.php
@@ -8,6 +8,9 @@
  * - https://bugs.php.net/bug.php?id=45735 (this has the most detailed explanation)
  * - https://bugs.php.net/bug.php?id=61744
  * - https://bugs.php.net/bug.php?id=71720
+ *
+ * As a summary, pcre matches every item into `$0`,if count reach the limit `pcre.backtrack_limit`
+ * you may got the `Segfault` which stop the whole process.
  */
 
 preg_match("/(0)*/", str_repeat("0", 15000));
